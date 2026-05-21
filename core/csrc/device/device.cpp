a4cd41976ce0dc63baeb7f6d7a80111d45e047c1
100018556-Li Haoyan <1620774239@qq.com>
2026-05-21 14:23:06 +0800

    test: am-test

--- core/csrc/device/device.cpp
+++ core/csrc/device/device.cpp
@@ -3,8 +3,6 @@
 
 #define TIMER_HZ 60
 
-void init_vga();
-void vga_update_screen();
 uint64_t get_time();
 
 void device_update() { 
@@ -15,20 +13,7 @@ void device_update() {
 	}
 	last = now;
 
-	IFDEF(CONFIG_HAS_VGA, vga_update_screen()); 
-#ifdef CONFIG_HAS_VGA
-	SDL_Event event;
-	while (SDL_PollEvent(&event)) {
-		switch (event.type) {
-			case SDL_QUIT:
-				npc_state.state = NPC_QUIT;
-				break;
-			default: break;
-		}
-	}
-#endif
 }
 
 void init_device() { 
-	IFDEF(CONFIG_HAS_VGA, init_vga());
 }
---
+++ set_env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+CURRENT_DIR=$(pwd)
+
+export CORE_HOME=$CURRENT_DIR/core
+export AM_HOME=$CURRENT_DIR/abstract-machine
